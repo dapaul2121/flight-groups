f6563dfae08eac5191a27750d6eb93714360f612
David Paul <davidpaul@Davids-MacBook-Pro.local>
2020-02-29 10:23:39 -0800

    basic app layout created

---
+++ server/index.js
@@ -0,0 +1,12 @@
+const express = require('express')
+const app = express()
+const port = 3000
+const path = require('path');
+const morgan = require('morgan');
+
+app.use(express.json());
+app.use(morgan('dev'));
+
+app.use(express.static(path.join(__dirname, '../client/dist')));
+
+app.listen(port, () => console.log(`listening on port ${port}!`));
