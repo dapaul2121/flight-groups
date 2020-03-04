7869338ab8f3ef03da153218c8cdbd3f4f7b2ac8
David Paul <davidpaul@Davids-MacBook-Pro.local>
2020-03-03 19:17:16 -0800

    empty state added

---
+++ mockData/appStateEmpty.js
@@ -0,0 +1,25 @@
+module.exports = {
+    groups: [{
+        _id: null,
+        name: '',
+        destinations: {},
+        datesFree: {
+            0: {}
+        },
+        members: [
+            {
+                firstName: '',
+                lastName: '',
+                email: '',
+                destinations: {
+                },
+                datesFree: {
+                    0: {}
+                },
+            }
+        ],
+        flights: []
+    }],
+    selectedGroup: 0,
+    currentView: 'main'
+}
